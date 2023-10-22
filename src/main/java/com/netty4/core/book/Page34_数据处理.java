dcd78e638ed5947e873b23853c9a34ac0f08cfda
Memo <2415782224@qq.com>
2023-10-22 18:04:28 +0800

    Initial commit

---
+++ src/main/java/com/netty4/core/book/Page34_数据处理.java
@@ -0,0 +1,24 @@
+package com.netty4.core.book;
+
+import java.io.*;
+
+/**
+ * @author: Rrow
+ * @date: 2023/10/22 17:54
+ * Description:
+ */
+public class Page34_数据处理 {
+    public static void main(String[] args) throws IOException {
+        // 文件输入流
+        FileInputStream inputStream = new FileInputStream("D:\\IDECodeProject\\filetest\\info.txt");
+        // 文件输入字符流
+        InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+        // 缓冲字符流
+        BufferedReader reader = new BufferedReader(inputStreamReader);
+        for (int i = 0; i < 4; i++) {
+            // 读取下一行数据
+            String readLine = reader.readLine();
+            System.out.println(readLine);
+        }
+    }
+}
